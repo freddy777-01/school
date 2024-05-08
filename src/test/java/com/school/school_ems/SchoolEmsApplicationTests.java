75504b8c857112b339a5f7017c1ee00755b0b348
MK <58578902+freddy777-01@users.noreply.github.com>
2024-05-08 17:49:10 +0300

    initial setup

---
+++ src/test/java/com/school/school_ems/SchoolEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.school_ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolEmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
