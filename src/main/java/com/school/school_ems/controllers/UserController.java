8f34c0885d06a870e1498d10ff782fcb9331160a
MK <58578902+freddy777-01@users.noreply.github.com>
2024-05-26 12:36:30 +0300

    on-progress: fixed and added cross site origins 4

--- src/main/java/com/school/school_ems/controllers/UserController.java
+++ src/main/java/com/school/school_ems/controllers/UserController.java
@@ -2,7 +2,7 @@ package com.school.school_ems.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import com.school.school_ems.models.User;
 import com.school.school_ems.services.TeacherService;
 import com.school.school_ems.services.UserService;
 
-import lombok.AllArgsConstructor;
+// import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
